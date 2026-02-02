bd620225504044a08c56d58cd473a2953c9e1977
renato <ren.rib.silva@gmail.com>
2026-02-01 22:15:44 -0300

    arquivos modificados

---
+++ exploration/2024/socials.R
@@ -0,0 +1,21 @@
+#--------------------------------
+# Importa os dados.csv -
+#--------------------------------
+
+filtered <- fread("exploration/2023/MICRODADOS/at_least_one_presence.csv")
+
+#-----------------------------
+# Caminho para gravar o json -
+#-----------------------------
+
+path_json <- as.character("~/Área\ de\ trabalho/DEV/NEXT/microenem/src/app/(home)/JSON/2023/visao-geral/socials/")
+
+#----------------------------------------------------
+# Frequência absoluta e relativa das faixas etárias -
+#----------------------------------------------------
+
+write_fx_etaria(filtered, path_json = path_json)
+
+write_sexo(filtered, path_json = path_json)
+
+write_cor_raca(filtered, path_json = path_json)
